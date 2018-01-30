49a4c8aec0c8f2f11a2031acb7250535331d491d
Golo Roden <golo.roden@thenativeweb.io>
2018-01-30 11:49:38 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,112 @@
+# @sealsystems/failure
+
+Easy handling of error codes and metadata.
+
+## Installation
+
+```bash
+npm install @sealsystems/failure
+```
+
+## Quick start
+
+First you need to add a reference to @sealsystems/failure within your application.
+
+```javascript
+const failure = require('@sealsystems/failure');
+```
+
+Then you can create an error object.
+
+```javascript
+const err = failure(42, 'Universal error.', { username: 'hugo' });
+```
+
+This creates an object of type `Error` with some additional properties:
+
+```javascript
+{
+  name: 'SealError',
+  code: 42,
+  message: 'Universal error',
+  metadata: {
+    username: 'hugo'
+  }
+  ...
+}
+```
+
+## API
+
+### Creating an error objects
+
+Create a new object of type `Error`.
+
+```javascript
+const error = failure(code, message, metadata);
+```
+
+Parameter:
+```
+code       number   optional    error code
+message    string   mandatory   error message
+metadata   object   optional    error metadata
+```
+
+Result: new error object
+
+### Join metadata
+
+Join metadata and error object metadata into a new metadata object for extended logging.
+
+**Attention**: This function does not alter the errors metadata, it just returns a new metadata object.
+
+```javascript
+const joinedMeta = failure.joinMeta(error, metadata);
+```
+
+Parameter:
+```
+error      object   mandatory   previously created error object
+metadata   object   mandatory   additional metadata to join
+```
+
+Result: new metadata object
+
+### Export plain object
+
+Export a plain new object for return in REST-API.
+
+```javascript
+const plainNewObject = failure.httpExport(error);
+```
+
+Parameter:
+```
+error      object   mandatory   previously created error object
+```
+
+Result: new plain javascript object of this structure:
+
+```javascript
+{
+  code: error.code,
+  message: error.message,
+  metadata: error.metadata
+}
+```
+
+### Export JSON string
+
+Export plain error object as JSON string for return in REST-API without bodyparser.
+
+```javascript
+const stringifiedObject = failure.jsonHttpExport(error);
+```
+
+Parameter:
+```
+error      object   mandatory   previously created error object
+```
+
+Result: JSON string of plain object created by `httpExport`
---
+++ test/failureTest.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const assert = require('assertthat');
+
+const failure = require('../lib/failure');
+
+suite('fileupload.failure', () => {
+  test('is a function', (done) => {
+    assert.that(failure).is.ofType('function');
+    done();
+  });
+
+  test('throws error if no message if given', (done) => {
+    assert.that(() => {
+      failure();
+    }).is.throwing('Message is missing.');
+    done();
+  });
+
+  test('throws error if code is not a number', (done) => {
+    assert.that(() => {
+      failure('asdf', 'huhu');
+    }).is.throwing('Illegal data type, "code" should be number.');
+    done();
+  });
+
+  test('returns SealError with default values', (done) => {
+    const err = failure('huhu');
+
+    assert.that(err).is.not.null();
+    assert.that(err.name).is.equalTo('SealError');
+    assert.that(err.message).is.equalTo('huhu');
+    assert.that(err.code).is.equalTo(0);
+    assert.that(err.metadata).is.equalTo({});
+    done();
+  });
+
+  test('returns SealError with given values', (done) => {
+    const err = failure(123, 'hopperla', { user: 'hugo' });
+
+    assert.that(err).is.not.null();
+    assert.that(err.name).is.equalTo('SealError');
+    assert.that(err.message).is.equalTo('hopperla');
+    assert.that(err.code).is.equalTo(123);
+    assert.that(err.metadata).is.equalTo({ user: 'hugo' });
+    done();
+  });
+
+  test('converts code of type string', (done) => {
+    const err = failure('456', 'einstring');
+
+    assert.that(err).is.not.null();
+    assert.that(err.name).is.equalTo('SealError');
+    assert.that(err.message).is.equalTo('einstring');
+    assert.that(err.code).is.equalTo(456);
+    assert.that(err.metadata).is.equalTo({});
+    done();
+  });
+
+  test('has httpExport function', (done) => {
+    assert.that(failure.httpExport).is.ofType('function');
+    done();
+  });
+
+  test('httpExport exports plain object with properties', (done) => {
+    const err = failure(123, 'hopperla', { user: 'hugo' });
+
+    assert.that(failure.httpExport(err)).is.equalTo({
+      message: 'hopperla',
+      code: 123,
+      metadata: {
+        user: 'hugo'
+      }
+    });
+    done();
+  });
+
+  test('has jsonHttpExport function', (done) => {
+    assert.that(failure.jsonHttpExport).is.ofType('function');
+    done();
+  });
+
+  test('jsonHttpExport exports JSON string with properties', (done) => {
+    const err = failure(123, 'hopperla', { user: 'hugo' });
+
+    assert.that(failure.jsonHttpExport(err)).is.equalTo(JSON.stringify({
+      code: 123,
+      message: 'hopperla',
+      metadata: {
+        user: 'hugo'
+      }
+    }));
+    done();
+  });
+
+  test('has joinMeta function', (done) => {
+    assert.that(failure.joinMeta).is.ofType('function');
+    done();
+  });
+
+  test('joinMeta returns enhanced metadata', (done) => {
+    const err = failure(123, 'hopperla', { user: 'hugo' });
+    const meta = failure.joinMeta(err, {
+      document: 'nixda'
+    });
+
+    assert.that(meta).is.equalTo({
+      user: 'hugo',
+      document: 'nixda'
+    });
+    done();
+  });
+});
