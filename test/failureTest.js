52d5e739dbdf688990cb0b2654ac4140667f0bed
seal-mt <manfred.thein@sealsystems.de>
2018-08-30 17:35:48 +0200

    wip

--- test/failureTest.js
+++ test/failureTest.js
@@ -2,29 +2,44 @@
 
 const assert = require('assertthat');
 
-const failure = require('../lib/failure');
+const SealError = require('../lib/failure');
 
 suite('failure', () => {
-  test('is a function', async () => {
-    assert.that(failure).is.ofType('function');
+  let plainResult;
+
+  setup(async () => {
+    plainResult = {
+      code: 123,
+      message: 'hopperla',
+      metadata: {
+        user: 'hugo',
+        kbcode: '@sealsystems/failure.undefined'
+      }
+    };
   });
 
+  test('is a function', async () => {
+    assert.that(SealError).is.ofType('function');
+  });
+
+  /* eslint-disable no-new */
   test('throws error if no message if given', async () => {
     assert.that(() => {
-      failure();
+      new SealError();
       throw new Error('X');
     }).is.throwing('Message is missing.');
   });
 
   test('throws error if code is not a number', async () => {
     assert.that(() => {
-      failure('asdf', 'huhu');
+      new SealError('asdf', 'huhu');
       throw new Error('X');
     }).is.throwing('Illegal data type, "code" should be number.');
   });
+  /* eslint-enable no-new */
 
   test('returns SealError with default values', async () => {
-    const err = failure('huhu');
+    const err = new SealError('huhu');
 
     assert.that(err).is.not.null();
     assert.that(err.name).is.equalTo('SealError');
@@ -36,7 +51,7 @@ suite('failure', () => {
   });
 
   test('returns SealError with all given values', async () => {
-    const err = failure('hopperla', 123, { user: 'hugo' });
+    const err = new SealError('hopperla', 123, { user: 'hugo' });
 
     assert.that(err).is.not.null();
     assert.that(err.name).is.equalTo('SealError');
@@ -49,7 +64,7 @@ suite('failure', () => {
   });
 
   test('returns SealError with given code only', async () => {
-    const err = failure('hopperla2', 666);
+    const err = new SealError('hopperla2', 666);
 
     assert.that(err).is.not.null();
     assert.that(err.name).is.equalTo('SealError');
@@ -61,7 +76,7 @@ suite('failure', () => {
   });
 
   test('returns SealError with given metadata only', async () => {
-    const err = failure('hopperla3', { test: true });
+    const err = new SealError('hopperla3', { test: true });
 
     assert.that(err).is.not.null();
     assert.that(err.name).is.equalTo('SealError');
@@ -74,7 +89,7 @@ suite('failure', () => {
   });
 
   test('returns SealError with kbcode from errors.js', async () => {
-    const err = failure('Chainable object is missing.', { user: 'hansi' });
+    const err = new SealError('Chainable object is missing.', { user: 'hansi' });
 
     assert.that(err).is.not.null();
     assert.that(err.name).is.equalTo('SealError');
@@ -87,8 +102,8 @@ suite('failure', () => {
   });
 
   test('chains errors', async () => {
-    const err1 = failure('Chainable object is missing.', { user: 'hansi' });
-    const err2 = failure('Message is missing.').chain(err1);
+    const err1 = new SealError('Chainable object is missing.', { user: 'hansi' });
+    const err2 = new SealError('Message is missing.').chain(err1);
 
     assert.that(err2.name).is.equalTo('SealError');
     assert.that(err2.message).is.equalTo('Message is missing.');
@@ -106,54 +121,66 @@ suite('failure', () => {
     });
   });
 
+  test('requires map only once', async () => {
+    const module = require('../package.json').name;
+    const tmpMap = SealError.prototype.errorsMap[module];
+
+    delete SealError.prototype.errorsMap[module];
+    /* eslint-disable no-new */
+    assert.that(Object.keys(SealError.prototype.errorsMap).length).is.equalTo(0);
+    new SealError('err1');
+    assert.that(Object.keys(SealError.prototype.errorsMap).length).is.equalTo(1);
+    assert.that(SealError.prototype.errorsMap[module]).is.equalTo(tmpMap);
+
+    SealError.prototype.errorsMap[module] = { test: true };
+    new SealError('err2');
+    assert.that(Object.keys(SealError.prototype.errorsMap).length).is.equalTo(1);
+    assert.that(SealError.prototype.errorsMap[module]).is.equalTo({ test: true });
+
+    SealError.prototype.errorsMap[module] = tmpMap;
+    /* eslint-enable no-new */
+  });
+
   test('throws error if no error object is given', async () => {
     assert.that(() => {
-      failure('huhu').chain();
+      new SealError('huhu').chain();
       throw new Error('X');
     }).is.throwing('Chainable object is missing.');
   });
 
-  test('has httpExport function', async () => {
-    assert.that(failure.httpExport).is.ofType('function');
+  test('has static plain function', async () => {
+    assert.that(SealError.plain).is.ofType('function');
   });
 
-  test('httpExport exports plain object with properties', async () => {
-    const err = failure('hopperla', 123, { user: 'hugo' });
+  test('returns plain object with properties', async () => {
+    const err = new SealError('hopperla', 123, { user: 'hugo' });
 
-    assert.that(failure.httpExport(err)).is.equalTo({
-      message: 'hopperla',
-      code: 123,
-      metadata: {
-        user: 'hugo',
-        kbcode: '@sealsystems/failure.undefined'
-      }
-    });
+    assert.that(SealError.plain(err)).is.equalTo(plainResult);
+    assert.that(err.normalize()).is.equalTo(plainResult);
   });
 
-  test('has jsonHttpExport function', async () => {
-    assert.that(failure.jsonHttpExport).is.ofType('function');
+  test('returns plain object with default properties', async () => {
+    const err = new Error('huch');
+
+    plainResult = {
+      code: 0,
+      message: 'huch',
+      metadata: {}
+    };
+    assert.that(SealError.plain(err)).is.equalTo(plainResult);
   });
 
-  test('jsonHttpExport exports JSON string with properties', async () => {
-    const err = failure('hopperla', 123, { user: 'hugo' });
+  test('returns JSON string with properties', async () => {
+    const err = new SealError('hopperla', 123, { user: 'hugo' });
+    const stringified = err.stringify();
+    const err2 = JSON.parse(stringified);
 
-    assert.that(failure.jsonHttpExport(err)).is.equalTo(JSON.stringify({
-      code: 123,
-      message: 'hopperla',
-      metadata: {
-        user: 'hugo',
-        kbcode: '@sealsystems/failure.undefined'
-      }
-    }));
+    assert.that(err2).is.equalTo(plainResult);
   });
 
-  test('has joinMeta function', async () => {
-    assert.that(failure.joinMeta).is.ofType('function');
-  });
-
-  test('joinMeta returns enhanced metadata', async () => {
-    const err = failure('hopperla', 123, { user: 'hugo' });
-    const meta = failure.joinMeta(err, {
+  test('returns enhanced metadata for logging', async () => {
+    const err = new SealError('hopperla', 123, { user: 'hugo' });
+    const meta = err.joinMeta({
       document: 'nixda'
     });
 
@@ -162,21 +189,5 @@ suite('failure', () => {
       document: 'nixda',
       kbcode: '@sealsystems/failure.undefined'
     });
-  });
-
-  test('isFailure returns false for undefined values', async () => {
-    assert.that(failure.isFailure()).is.false();
-  });
-
-  test('isFailure returns false if name is not defined', async () => {
-    assert.that(failure.isFailure({})).is.false();
-  });
-
-  test('isFailure returns false if object is not inherited from SealFailure', async () => {
-    assert.that(failure.isFailure({ name: 'SealError' })).is.false();
-  });
-
-  test('isFailure returns true if value is a failure generated SealError', async () => {
-    assert.that(failure.isFailure(failure('huhu', 1))).is.true();
   });
 });
